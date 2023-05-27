3a120474373fd650d07129266d7dc71da0048721
Lukas <lukasdrinkwater97@gmail.com>
2023-05-27 21:25:52 +0100

    follow lesson

---
+++ efficiency-basics/javasript.js
@@ -0,0 +1,25 @@
+function oddNumbersLessThanTen() {
+  let currentNumber = 1;
+
+  while (currentNumber < 10) {
+    if (currentNumber % 2 !== 0) {
+      console.log(currentNumber);
+    }
+
+    currentNumber += 1;
+  }
+}
+
+// oddNumbersLessThanTen();
+
+function oddNumbers(maxNumber) {
+  let currentNumber = 1;
+
+  while (currentNumber < maxNumber) {
+    if (currentNumber % 2 !== 0) {
+      console.log(currentNumber);
+    }
+
+    currentNumber += 1;
+  }
+}
